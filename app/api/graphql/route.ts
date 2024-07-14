8c5c22db83c9663e5f011c27921889169da4d017
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-07-14 18:41:05 +0530

    create graphql api route

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -1,9 +1,45 @@
+import { serverClient } from "@/lib/server/serverClient";
+import { gql } from "@apollo/client";
+import { NextResponse } from "next/server";
+
+const corsHeaders = {
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+	"Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
 export async function POST(request: Request) {
 	const { query, variables } = await request.json();
 
 	try {
+		let result;
+
 		if (query.trim().startsWith("mutation")) {
 			// Handle mutation
+			result = await serverClient.mutate({
+				mutation: gql`${query}`,
+				variables,
+			});
+		} else {
+			// Handle queries
+			result = await serverClient.query({
+				query: gql`${query}`,
+				variables,
+			});
 		}
-	} catch (error) {}
+
+		const data = result.data;
+
+		return NextResponse.json(
+			{
+				data,
+			},
+			{ headers: corsHeaders }
+		);
+	} catch (error) {
+		console.log(error);
+		return NextResponse.json(error, {
+			status: 500,
+		});
+	}
 }
