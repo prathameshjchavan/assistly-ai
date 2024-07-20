4bf53edaa13d7531d329cb21cd36fd2bce533434
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-07-20 18:49:12 +0530

    get chat session messages

--- app/(admin)/review-sessions/[id]/page.tsx
+++ app/(admin)/review-sessions/[id]/page.tsx
@@ -1,7 +1,40 @@
-const ReviewSessionPage = () => {
-  return (
-    <div>ReviewSessionPage</div>
-  )
+import { GET_CHAT_SESSION_MESSAGES } from "@/graphql/queries";
+import { serverClient } from "@/lib/server/serverClient";
+import {
+	GetChatSessionMessagesResponse,
+	GetChatSessionMessagesVariables,
+} from "@/types/types";
+
+export const dynamic = "force-dynamic";
+
+interface ReviewSessionPageProps {
+	params: {
+		id: string;
+	};
 }
 
-export default ReviewSessionPage
+const ReviewSessionPage = async ({
+	params: { id },
+}: ReviewSessionPageProps) => {
+	const {
+		data: {
+			chat_sessions: {
+				id: chatSessionId,
+				created_at,
+				messages,
+				chatbots: { name },
+				guests: { name: guestName, email },
+			},
+		},
+	} = await serverClient.query<
+		GetChatSessionMessagesResponse,
+		GetChatSessionMessagesVariables
+	>({
+		query: GET_CHAT_SESSION_MESSAGES,
+		variables: { id: parseInt(id) },
+	});
+
+	return <div>ReviewSessionPage</div>;
+};
+
+export default ReviewSessionPage;
--- components/ChatbotSessions.tsx
+++ components/ChatbotSessions.tsx
@@ -27,55 +27,57 @@ const ChatbotSessions = ({ chatbots }: ChatbotSessions) => {
 	}, [chatbots]);
 
 	return (
-		<Accordion type="single" collapsible>
-			{sortedChatbots.map((chatbot) => {
-				const hasSessions = !!chatbot.chat_sessions.length;
-				return (
-					<AccordionItem
-						key={chatbot.id}
-						value={`item-${chatbot.id}`}
-						className="px-10 py-5"
-					>
-						{hasSessions ? (
-							<Fragment>
-								<AccordionTrigger>
-									<div className="flex text-left items-center w-full">
-										<Avatar seed={chatbot.name} className="h-10 w-10 mr-4" />
-										<div className="flex flex-1 justify-between space-x-4">
-											<p>{chatbot.name}</p>
-											<p className="pr-4 font-bold text-right">
-												{chatbot.chat_sessions.length} sessions
-											</p>
+		<div className="bg-white">
+			<Accordion type="single" collapsible>
+				{sortedChatbots.map((chatbot) => {
+					const hasSessions = !!chatbot.chat_sessions.length;
+					return (
+						<AccordionItem
+							key={chatbot.id}
+							value={`item-${chatbot.id}`}
+							className="px-10 py-5"
+						>
+							{hasSessions ? (
+								<Fragment>
+									<AccordionTrigger>
+										<div className="flex text-left items-center w-full">
+											<Avatar seed={chatbot.name} className="h-10 w-10 mr-4" />
+											<div className="flex flex-1 justify-between space-x-4">
+												<p>{chatbot.name}</p>
+												<p className="pr-4 font-bold text-right">
+													{chatbot.chat_sessions.length} sessions
+												</p>
+											</div>
 										</div>
-									</div>
-								</AccordionTrigger>
-								<AccordionContent className="space-y-5 p-5 bg-gray-100 rounded-md">
-									{chatbot.chat_sessions.map((session) => (
-										<Link
-											href={`/review-sessions/${session.id}`}
-											key={session.id}
-											className="relative p-10 bg-[#2991EE] text-white rounded-md block"
-										>
-											<p className="text-lg font-bold">
-												{session.guest?.name || "Anonymous"}
-											</p>
-											<p className="text-sm font-light">
-												{session.guest?.email || "No email provided"}
-											</p>
-											<p className="absolute top-5 right-5 text-sm">
-												<ReactTimeago date={new Date(session.created_at)} />
-											</p>
-										</Link>
-									))}
-								</AccordionContent>
-							</Fragment>
-						) : (
-							<p className="font-light">{chatbot.name} (No Sessions)</p>
-						)}
-					</AccordionItem>
-				);
-			})}
-		</Accordion>
+									</AccordionTrigger>
+									<AccordionContent className="space-y-5 p-5 bg-gray-100 rounded-md">
+										{chatbot.chat_sessions.map((session) => (
+											<Link
+												href={`/review-sessions/${session.id}`}
+												key={session.id}
+												className="relative p-10 bg-[#2991EE] text-white rounded-md block"
+											>
+												<p className="text-lg font-bold">
+													{session.guest?.name || "Anonymous"}
+												</p>
+												<p className="text-sm font-light">
+													{session.guest?.email || "No email provided"}
+												</p>
+												<p className="absolute top-5 right-5 text-sm">
+													<ReactTimeago date={new Date(session.created_at)} />
+												</p>
+											</Link>
+										))}
+									</AccordionContent>
+								</Fragment>
+							) : (
+								<p className="font-light">{chatbot.name} (No Sessions)</p>
+							)}
+						</AccordionItem>
+					);
+				})}
+			</Accordion>
+		</div>
 	);
 };
 
