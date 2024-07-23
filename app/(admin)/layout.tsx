f4adab5755d02c4d60ff2ec7d4ffccac99e4bab1
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-07-23 12:04:28 +0530

    add login page

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,12 +1,18 @@
 import Header from "@/components/Header";
 import Sidebar from "@/components/Sidebar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
 interface AdminLayoutProps {
 	children: ReactNode;
 }
 
-const AdminLayout = ({ children }: AdminLayoutProps) => {
+const AdminLayout = async ({ children }: AdminLayoutProps) => {
+	const { userId } = await auth();
+
+	if (!userId) return redirect("/login");
+
 	return (
 		<div className="flex flex-col flex-1">
 			<Header />
