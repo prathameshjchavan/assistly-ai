0e39571aa509a2d19a0d60c21312e30b78c9bc65
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-07-23 13:37:35 +0530

    add loading page

---
+++ app/(admin)/loading.tsx
@@ -0,0 +1,11 @@
+import Avatar from "@/components/Avatar";
+
+const LoadingPage = () => {
+	return (
+		<div className="mx-auto animate-spin p-10">
+			<Avatar seed="PAPAFAM Support Agent" />
+		</div>
+	);
+};
+
+export default LoadingPage;
